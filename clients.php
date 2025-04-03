4f47a65e3c5b1b93ecc4615fc1239228a7529ad7
codystack <ebuzzadvert@gmail.com>
2025-04-03 17:30:26 +0100

    clients added

--- clients.php
+++ clients.php
@@ -13,13 +13,10 @@ include "./components/navbar.php";
                         <div class="row">
                             <div class="col-xl-12">
                                 <div class="tm-hero-content">
-                                    <h4 class="tm-hero-title tp-char-animation">Our Sponsors</h4>
+                                    <h4 class="tm-hero-title tp-char-animation">Our Clients</h4>
                                 </div>
                                 <div class="tm-hero-text tp_title_anim">
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing
-                                        elit, orbi egestas lacus ac suscipit ovallis.
-                                    </p>
+                                    <p>Trusted by these incredible clients. We're proud to partner with them on their journey to success.</p>
                                 </div>
                             </div>
                         </div>
@@ -29,17 +26,212 @@ include "./components/navbar.php";
                 <div class="tp-brand-4-area pt-20 pb-120">
                     <div class="container">
                         <div class="row gx-0">
+                        <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/skinicon.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>skinicon.ng</span>
+                                        <span>skinicon.ng</span>
+                                        <span>skinicon.ng</span>
+                                        <span>skinicon.ng</span>
+                                        <span>skinicon.ng</span>
+                                        <span>skinicon.ng</span>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="col-xl-3 col-lg-3 col-md-6">
                                 <div class="tp-brand-4-item p-relative">
-                                <img src="assets/img/home-04/brand/brand-1.png" alt="">
-                                <div class="tp-brand-4-line-text d-flex align-items-center">
-                                    <span>TopoChico.com</span>
-                                    <span>TopoChico.com</span>
-                                    <span>TopoChico.com</span>
-                                    <span>TopoChico.com</span>
-                                    <span>TopoChico.com</span>
-                                    <span>TopoChico.com</span>
+                                    <img src="assets/img/client/billbuddy.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>billbuddy.africa</span>
+                                        <span>billbuddy.africa</span>
+                                        <span>billbuddy.africa</span>
+                                        <span>billbuddy.africa</span>
+                                        <span>billbuddy.africa</span>
+                                        <span>billbuddy.africa</span>
+                                    </div>
                                 </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/quickfix.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>quickfix.ng</span>
+                                        <span>quickfix.ng</span>
+                                        <span>quickfix.ng</span>
+                                        <span>quickfix.ng</span>
+                                        <span>quickfix.ng</span>
+                                        <span>quickfix.ng</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/twj.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>twjhub.com</span>
+                                        <span>twjhub.com</span>
+                                        <span>twjhub.com</span>
+                                        <span>twjhub.com</span>
+                                        <span>twjhub.com</span>
+                                        <span>twjhub.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/momos.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>momostula.com</span>
+                                        <span>momostula.com</span>
+                                        <span>momostula.com</span>
+                                        <span>momostula.com</span>
+                                        <span>momostula.com</span>
+                                        <span>momostula.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/vineyard.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>thevineyard.ng</span>
+                                        <span>thevineyard.ng</span>
+                                        <span>thevineyard.ng</span>
+                                        <span>thevineyard.ng</span>
+                                        <span>thevineyard.ng</span>
+                                        <span>thevineyard.ng</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/phfw.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>portharcourtfashionweek.com</span>
+                                        <span>portharcourtfashionweek.com</span>
+                                        <span>portharcourtfashionweek.com</span>
+                                        <span>portharcourtfashionweek.com</span>
+                                        <span>portharcourtfashionweek.com</span>
+                                        <span>portharcourtfashionweek.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/chaelboreh.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>chaelboreh.com</span>
+                                        <span>chaelboreh.com</span>
+                                        <span>chaelboreh.com</span>
+                                        <span>chaelboreh.com</span>
+                                        <span>chaelboreh.com</span>
+                                        <span>chaelboreh.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/prohelp.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>prohelp.ng</span>
+                                        <span>prohelp.ng</span>
+                                        <span>prohelp.ng</span>
+                                        <span>prohelp.ng</span>
+                                        <span>prohelp.ng</span>
+                                        <span>prohelp.ng</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/rsphcmbx.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>rsphcmb.rv.gov.ng</span>
+                                        <span>rsphcmb.rv.gov.ng</span>
+                                        <span>rsphcmb.rv.gov.ng</span>
+                                        <span>rsphcmb.rv.gov.ng</span>
+                                        <span>rsphcmb.rv.gov.ng</span>
+                                        <span>rsphcmb.rv.gov.ng</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/voteonline.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>voteonline.com.ng</span>
+                                        <span>voteonline.com.ng</span>
+                                        <span>voteonline.com.ng</span>
+                                        <span>voteonline.com.ng</span>
+                                        <span>voteonline.com.ng</span>
+                                        <span>voteonline.com.ng</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/tubo.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>tubodavids.co.uk</span>
+                                        <span>tubodavids.co.uk</span>
+                                        <span>tubodavids.co.uk</span>
+                                        <span>tubodavids.co.uk</span>
+                                        <span>tubodavids.co.uk</span>
+                                        <span>tubodavids.co.uk</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/fastquid.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>fastquid.ng</span>
+                                        <span>fastquid.ng</span>
+                                        <span>fastquid.ng</span>
+                                        <span>fastquid.ng</span>
+                                        <span>fastquid.ng</span>
+                                        <span>fastquid.ng</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/quickpocket.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>quickpocket.co</span>
+                                        <span>quickpocket.co</span>
+                                        <span>quickpocket.co</span>
+                                        <span>quickpocket.co</span>
+                                        <span>quickpocket.co</span>
+                                        <span>quickpocket.co</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/eskrow.png" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>eskrow.co</span>
+                                        <span>eskrow.co</span>
+                                        <span>eskrow.co</span>
+                                        <span>eskrow.co</span>
+                                        <span>eskrow.co</span>
+                                        <span>eskrow.co</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-6">
+                                <div class="tp-brand-4-item p-relative">
+                                    <img src="assets/img/client/bgh.png" width="100" alt="">
+                                    <div class="tp-brand-4-line-text d-flex align-items-center">
+                                        <span>bouquiglamhouse.co.uk</span>
+                                        <span>bouquiglamhouse.co.uk</span>
+                                        <span>bouquiglamhouse.co.uk</span>
+                                        <span>bouquiglamhouse.co.uk</span>
+                                        <span>bouquiglamhouse.co.uk</span>
+                                        <span>bouquiglamhouse.co.uk</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
