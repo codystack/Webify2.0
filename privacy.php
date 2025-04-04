f85fedcde14dd6b12e637b9ba18f52e6d6e995bc
codystack <ebuzzadvert@gmail.com>
2025-04-04 08:03:40 +0100

    policies added

---
+++ privacy.php
@@ -0,0 +1,68 @@
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+
+            <main>
+
+                <div class="tm-hero-area tm-hero-ptb">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="tm-hero-content">
+                                    <h4 class="tm-hero-title tp-char-animation">Privacy policy</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xxl-12 col-xl-12 col-lg-12">
+                            <div class="postbox__wrapper">
+                                <div class="blog-details-left-content">
+                                    <h4 class="blog-details-left-title">Introduction</h4>
+                                    <p class="mb-30">Your privacy is important to us. This privacy policy explains how we collect, use, and share your personal information when you visit our website or use our services.</p>
+                                    
+                                    <h4 class="blog-details-left-title">What information do we collect?</h4>
+                                    <p class="mb-30"><b>We collect the following information about you when you visit our website or use our services:</b><br>
+                                    Contact information: This includes your name, email address, phone number, and mailing address. Transaction information: This includes information about your purchases, such as your order number, shipping address, and payment information. Technical information: This includes information about your computer or device, such as your IP address, browser type, and operating system. Usage information: This includes information about how you use our website or services, such as the pages you visit and the links you click.</p>
+                                    
+                                    <h4 class="blog-details-left-title">How do we use your information?</h4>
+                                    <p class="mb-30"><b>We use your information for the following purposes:</b><br>
+                                    To provide you with our services: We use your information to provide you with the services you have requested, such as designing a website or creating marketing materials. To communicate with you: We use your information to communicate with you about your account, your orders, and other matters related to our services. To improve our services: We use your information to improve our website and services, such as by analyzing how you use our website and identifying areas where we can improve the user experience. To market our services: We may use your information to market our services to you, such as by sending you email newsletters or promotional materials.</p>
+
+                                    <h4 class="blog-details-left-title">How do we protect your information?</h4>
+                                    <p><b>We take measures to protect your information, such as:</b><br>
+                                    Encrypting your information: We encrypt your information when it is transmitted over the internet.
+                                    Storing your information securely: We store your information on secure servers.
+                                    Restricting access to your information: We restrict access to your information to authorized employees and contractors.</p>
+                                    <p class="mb-30">
+                                        <b>Your rights</b>
+                                        <b>You have the following rights with respect to your information:</b><br>
+                                        <b>Access:</b> You have the right to access your information.<br>
+                                        <b>Correction:</b> You have the right to correct any inaccurate information about you.<br>
+                                        <b>Deletion:</b> You have the right to delete your information.<br>
+                                        <b>Objection:</b> You have the right to object to the processing of your information.
+                                        Withdrawal of consent: You have the right to withdraw your consent to the processing of your information.
+                                    </p>
+
+                                    <h4 class="blog-details-left-title">How to contact us</h4>
+                                    <p class="mb-30">If you have any questions about this privacy policy, you can contact us at: <a href="mailto:info@webify.com.ng">info@webify.com.ng</a>
+                                    We may change this privacy policy from time to time. If we make any material changes, we will notify you by email or by posting a notice on our website.</p>
+
+                                    <h4 class="blog-details-left-title">Effective date</h4>
+                                    <p>This privacy policy is effective as of 2/05/2021.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
+<?php include "./components/footer.php"; ?>
---
+++ terms.php
@@ -0,0 +1,63 @@
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+
+            <main>
+
+                <div class="tm-hero-area tm-hero-ptb">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="tm-hero-content">
+                                    <h4 class="tm-hero-title tp-char-animation">Terms of use</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xxl-12 col-xl-12 col-lg-12">
+                            <div class="postbox__wrapper">
+                                <div class="blog-details-left-content">
+                                    <h4 class="blog-details-left-title">Introduction</h4>
+                                    <p class="mb-30">These terms and conditions govern the use of the website located at www.webify.com.ng (the “Website”). By using the Website, you agree to be bound by these terms and conditions.</p>
+                                    
+                                    <h4 class="blog-details-left-title">Definitions</h4>
+                                    <p class="mb-30">“Webify means the owner and operator of the Website.<br>
+                                    “User” means any person who accesses or uses the Website.</p>
+                                    
+                                    <h4 class="blog-details-left-title">Scope of Website</h4>
+                                    <p class="mb-30">The Website is a platform for Webify to showcase its design and web development services. The Website also provides users with information about Webify and its services.</p>
+
+                                    <h4 class="blog-details-left-title">User Conduct</h4>
+                                    <p class="mb-30">Users are prohibited from using the Website in any way that is unlawful, harmful, or disruptive. This includes, but is not limited to, the following: Posting or transmitting any unlawful, harmful, or disruptive content. Hacking into or otherwise attempting to gain unauthorized access to the Website. Distributing spam or other unsolicited or unauthorized messages. Violating the privacy of other users.</p>
+
+                                    <h4 class="blog-details-left-title">Intellectual Property</h4>
+                                    <p class="mb-30">All intellectual property rights in the Website, including but not limited to the text, images, and other content, are owned by Webify. Users are prohibited from copying, distributing, or modifying any of the content on the Website without the express written permission of Webify.</p>
+
+                                    <h4 class="blog-details-left-title">Changes to Website Terms and Conditions</h4>
+                                    <p class="mb-30">Webify may modify these terms and conditions at any time. The most current version of the terms and conditions will be posted on the Website. Users are responsible for reviewing the terms and conditions periodically to ensure that they are aware of any changes.</p>
+
+                                    <h4 class="blog-details-left-title">Governing Law</h4>
+                                    <p class="mb-30">These terms and conditions shall be governed by and construed in accordance with the laws of Nigeria.</p>
+
+                                    <h4 class="blog-details-left-title">Severability</h4>
+                                    <p class="mb-30">If any provision of these terms and conditions is held to be invalid or unenforceable, such provision shall be struck from these terms and conditions and the remaining provisions shall remain in full force and effect.</p>
+
+                                    <h4 class="blog-details-left-title">Waiver</h4>
+                                    <p class="mb-30">No waiver of any provision of these terms and conditions shall be effective unless in writing and signed by both parties.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
+<?php include "./components/footer.php"; ?>
